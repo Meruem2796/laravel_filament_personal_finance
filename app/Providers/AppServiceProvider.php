b64b44ad1f5ef74fb30cf6b4e8e5e8f5b6a83ca6
Abdou_Mourid <abdoumouhid96@yahoo.com>
2026-06-21 12:39:16 +0200

    Implement TransactionObserver to automatically update bank account balances on transaction CRUD operations and add corresponding feature tests

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,8 @@
 <?php
-
 namespace App\Providers;
 
+use App\Models\Transaction;
+use App\Observers\TransactionObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Transaction::observe(TransactionObserver::class);
     }
 }
